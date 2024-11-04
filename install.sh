79ebd48a65ad00bc223e5111cc6265a87d2b75fa
chengml <cheng.maolin@astute-tec.com>
2024-11-04 11:55:43 +0800

    Fix charts copying

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Prepare:
+# 1. ip and gateway configured
+# 2. ssh mutual authentication to all nodes
+
 # This base on rocky linux 9.2
 dnf install -y git python-pip
 # pip install ansible
@@ -10,10 +14,11 @@ pip install --trusted-host mirrors.aliyun.com -i http://mirrors.aliyun.com/pypi/
 # Establish ssh mutual trust from first server to other nodes
 for node in $(grep rke2_type hosts | awk -F'=' '{print $2}' | awk '{print $1}');do
     cat ~/.ssh/id_ed25519.pub | ssh $node "cat >> ~/.ssh/authorized_keys"
+    sed -i -e '/nameserver/d' -e 'a nameserver 1.1.1.1' /etc/resolv.conf
 done
 
 read -p"Continue to install(y/n)" answer
-if [[ "${answer}x" != "yx" -a "${answer}x" != "x" ]];then
+if [[ "${answer}x" != "yx" && "${answer}x" != "x" ]];then
     exit 0
 fi
 
@@ -21,7 +26,7 @@ fi
 ansible-playbook -i hosts site.yml
 
 # echo "export PATH=\$PATH:/var/lib/rancher/rke2/bin/" >> ~/.bashrc 
-if ! grep CRI_CONFIG_FILE ~/.bashrc;then
+if ! grep kubectl ~/.bashrc;then
     tee >> ~/.bashrc <<EOF
 alias kubectl='/var/lib/rancher/rke2/bin/kubectl --kubeconfig /etc/rancher/rke2/rke2.yaml'
 alias k='kubectl'
