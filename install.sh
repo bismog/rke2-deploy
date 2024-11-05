579abddc38a8d42267efa38bd886ab6b59eb6e28
chengml <cheng.maolin@astute-tec.com>
2024-11-05 11:27:06 +0800

    Configure ntp

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+workdir=$(cd $(dirname $0);pwd)
+
 # Establish ssh mutual trust from first server to other nodes
 for node in $(grep rke2_type hosts | awk -F'=' '{print $2}' | awk '{print $1}');do
     cat ~/.ssh/id_ed25519.pub | ssh $node "cat >> ~/.ssh/authorized_keys"
-    sed -i -e '/nameserver/d' -e 'a nameserver 1.1.1.1' /etc/resolv.conf
+    ssh $node "nmcli con mod bond-mgtbond ipv4.dns 1.1.1.1;nmcli con up bond-mgtbond"
+    # check: nmcli -g ipv4.dns connection show bond-mgtbond
+    #        cat /etc/resolv.conf
 done
 
 # This base on rocky linux 9.2
@@ -17,12 +21,4 @@ if [[ "${answer}x" != "yx" && "${answer}x" != "x" ]];then
 fi
 
 # Run deployment
-ansible-playbook -i hosts site.yml
-
-# echo "export PATH=\$PATH:/var/lib/rancher/rke2/bin/" >> ~/.bashrc 
-if ! grep kubectl ~/.bashrc;then
-    tee >> ~/.bashrc <<EOF
-alias kubectl='/var/lib/rancher/rke2/bin/kubectl --kubeconfig /etc/rancher/rke2/rke2.yaml'
-alias k='kubectl'
-EOF
-fi
+ansible-playbook -i hosts site.yml | tee $workdir/ansible.log
