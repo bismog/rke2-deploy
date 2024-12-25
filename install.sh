2d95f80b553bfb9fa3d7bace814507bd06c6fb49
chengml <cheng.maolin@astute-tec.com>
2024-12-25 14:24:31 +0800

    Enable multiple networks with multus and NAD

--- install.sh
+++ install.sh
@@ -11,11 +11,35 @@ for node in $(grep rke2_type hosts | awk -F'=' '{print $2}' | awk '{print $1}');
 done
 
 # This base on rocky linux 9.2
-dnf install -y git python-pip
-# pip install ansible
-pip install --trusted-host mirrors.aliyun.com -i http://mirrors.aliyun.com/pypi/simple ansible
+if ! rpm -q python3-pip;then
+    dnf install -y git python3-pip
+fi
 
-read -p"Continue to install(y/n)" answer
+if [[ ! -e ~/.pip/pip.conf ]];then
+    mkdir -p ~/.pip
+    tee ~/.pip/pip.conf <<EOF
+[global]
+retries=0
+index-url=http://mirrors.aliyun.com/pypi/simple/
+extra-index-url=
+        https://pypi.tuna.tsinghua.edu.cn/simple/
+        http://pypi.douban.com/simple
+        http://pypi.mirrors.ustc.edu.cn/simple/
+
+[install]
+trusted-host=
+        mirrors.aliyun.com
+        pypi.tuna.tsinghua.edu.cn
+        pypi.douban.com
+        pypi.mirrors.ustc.edu.cn
+EOF
+fi
+pip install -r requirements.txt
+
+# check list
+# iface2 should not in the same CIDR with iface1
+# definition in files: hosts and group_vars/all.yml
+read -p"Continue to install? (y/n)" answer
 if [[ "${answer}x" != "yx" && "${answer}x" != "x" ]];then
     exit 0
 fi
